8919a4473c2ed2820da38ae23b67a0826ffdd5eb
tpfennig <thomas.pfennig@simplesystem.com>
2019-03-15 13:41:27 +0100

    init commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# gentree
+
+[![Build Status](https://travis-ci.org/)](https://travis-ci.org/)
+[![Coverage Status](https://coveralls.io/repos/github/)](https://coveralls.io/github/)
+[![License](http://img.shields.io/:license-mit-blue.svg)](https://github.com/)
+
+
---
+++ backend/src/main/java/de/tpfennig/gentree/GentreeApplication.java
@@ -0,0 +1,13 @@
+package de.tpfennig.gentree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GentreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GentreeApplication.class, args);
+	}
+
+}
